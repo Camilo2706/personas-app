5333e0306c5e1c5e33e22a15ad2c51949db7964d
CAMILO HOYOS <camilohoyos2706@gmail.com>
2024-10-02 22:55:00 -0500

    Commune Edit

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@ route::get('/comunas',[ComunaController::class, 'index'])->name('comuna.index');
 route::post('/comunas',[ComunaController::class, 'store'])->name('comunas.store');
 route::get('/comunas/create',[ComunaController::class,'create'])->name('comunas.create');
 Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
+Route::put('/comunas/{comuna}',[ComunaController::class,'update'])->name('comunas.update');
+Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('comunas.edit');
