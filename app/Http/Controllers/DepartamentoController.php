7d159160b84e23ef8648b7f077aaedf662e84652
CAMILO HOYOS <camilohoyos2706@gmail.com>
2024-10-03 12:37:13 -0500

    DEPARTEMENTO ARREGLOS

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class DepartamentoController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -23,18 +23,36 @@ class DepartamentoController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        $paises = DB::table('tb_pais')
+        ->orderBy('pais_nomb')
+        ->get();
+
+    return view('departamentos.new', ['paises' => $paises]);
     }
 
     /**
      * Store a newly created resource in storage.
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
-        //
+        $departamento = new Departamento();
+    $departamento->depa_nomb = $request->depa_nomb;
+    $departamento->pais_codi = $request->pais_codi;
+    $departamento->save();
+
+    $departamentos = DB::table('tb_departamento')
+        ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+        ->select('tb_departamento.*', 'tb_pais.pais_nomb')
+        ->get();
+
+    return view('departamentos.index', ['departamentos' => $departamentos]);
     }
 
     /**
@@ -50,7 +68,15 @@ class DepartamentoController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $departamento = Departamento::find($id);
+
+ 
+    $paises = DB::table('tb_pais')
+        ->orderBy('pais_nomb')
+        ->get();
+
+   
+    return view('departamentos.edit', ['departamento' => $departamento, 'paises' => $paises]);
     }
 
     /**
@@ -58,14 +84,35 @@ class DepartamentoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $departamento = Departamento::find($id);
+        $departamento->depa_nomb = $request->depa_nomb;
+        $departamento->pais_codi = $request->pais_codi;
+        $departamento->save();
+    
+        $departamentos = DB::table('tb_departamento')
+            ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+            ->select('tb_departamento.*', 'tb_pais.pais_nomb')
+            ->get();
+    
+        return view('departamentos.index', ['departamentos' => $departamentos]);
     }
 
     /**
      * Remove the specified resource from storage.
+     * @param string $id
+     * @return \Illuminate\Http\Response
+     * 
      */
     public function destroy(string $id)
     {
-        //
+        $departamento = Departamento::find($id);
+        $departamento->delete();
+    
+        $departamentos = DB::table('tb_departamento')
+            ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+            ->select('tb_departamento.*', 'tb_pais.pais_nomb')
+            ->get();
+    
+        return view('departamentos.index', ['departamentos' => $departamentos]);
     }
 }
--- resources/views/departamentos/edit.blade.php
+++ resources/views/departamentos/edit.blade.php
@@ -1,46 +1,49 @@
 <!doctype html>
 <html lang="en">
-<head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Edit Departamento</title>
-</head>
-<body>
-<div class="container">
+  </head>
+  <body>
+    <div class= "container">
     <h1>Edit Departamento</h1>
-    <form method="POST" action="{{ route('departamentos.update', ['departamento' => $departamento->depa_codi]) }}">
-        @csrf
-        @method('PUT')
+    <form method="POST" action = "{{ route('departamentos.update',['departamento' => $departamento->depa_codi]) }}">
+    @method('put')
+    @csrf
+  <div class="mb-3">
+    <label for="codigo" class="form-label">Id</label>
+    <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id"
+    disabled= "disabled" value="{{ $departamento->depa_codi }}">
+    <div id="codigoHelp" class="form-text">Department Id</div>
+  </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Department</label>
+    <input type="text" class="form-control" id="name" placeholder="Department name"
+    name="depa_nomb" value="{{ $departamento->depa_nomb }}">
+  </div>
+  <label for="country">Country:</label>
+  <select class="form-select" id="country" name="pais_codi" required>
+    <option select disable value="">Choose one...</option>
+    @foreach ($paises as $pais)
+         @if ($pais->pais_codi == $departamento->pais_codi)
+             <option selected value="{{$pais->pais_codi}}">{{$pais->pais_nomb}}</option>
+         @else
+             <option value="{{$pais->pais_codi}}">{{$pais->pais_nomb}}</option>
+         @endif
+    @endforeach
+  </select>
+  <div class="mt-3">
+     <button type="submit" class="btn btn-primary">Update</button>
+     <a href="{{ route('departamentos.index')}}" class="btn btn-warning">Cancel</a>
+  </div>
+</form>
 
-        <div class="mb-3">
-            <label for="depa_codi" class="form-label">Code</label>
-            <input type="text" class="form-control" id="depa_codi" name="depa_codi" value="{{ $departamento->depa_codi }}" readonly>
-        </div>
-
-        <div class="mb-3">
-            <label for="depa_nomb" class="form-label">Departamento</label>
-            <input type="text" class="form-control" id="depa_nomb" name="depa_nomb" value="{{ $departamento->depa_nomb }}">
-        </div>
-
-        <label for="pais_codi">Country:</label>
-        <select class="form-select" id="pais_codi" name="pais_codi" required>
-            @foreach ($paises as $pais)
-                <option value="{{ $pais->pais_codi }}" {{ $pais->pais_codi == $departamento->pais_codi ? 'selected' : '' }}>
-                    {{ $pais->pais_nomb }}
-                </option>
-            @endforeach
-        </select>
-
-        <div class="mt-3">
-            <button type="submit" class="btn btn-primary">Update</button>
-            <a href="{{ route('departamentos.index') }}" class="btn btn-warning">Cancel</a>
-        </div>
-    </form>
-</div>
-</body>
+  </div>
+  </body>
 </html>
