3340a12f230300c9cdf2d828b9d322285a979b92
Camilo Hoyos <camilohoyos2706@gmail.com>
2024-11-18 23:45:56 -0500

    Municipio y Api

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ComunaController;
+use App\Http\Controllers\Api\MunicipioController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(ComunaController::class)->group(function () {
+    Route::get('/comuna', 'index');
+    Route::post('/comuna', 'store');
+    Route::get('/comuna/{id}', 'show');
+    Route::put('/comuna/{id}', 'update');
+    Route::delete('/comuna/{id}', 'destroy');
+});
+
+Route::controller(MunicipioController::class)->group(function () {
+    Route::get('/municipio', 'index');
+});
